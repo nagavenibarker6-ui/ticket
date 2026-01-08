1cd61f8d3d26ada838c203b40b7446408f2f6188
Nagaveni.Barker <nagavenibarker6@gmail.com>
2026-01-08 20:40:39 +0530

    Update movie.py

--- movie.py
+++ movie.py
@@ -1,4 +1,3 @@
-import sys
 def calculate_bill(prices, quantities):
     total = 0
     for p, q in zip(prices, quantities):
@@ -11,29 +10,12 @@ def booking_type(total_amount):
         return "Gold Booking"
     else:
         return "Silver Booking"
-def main():
-    if len(sys.argv) > 4:
-        customer_name = sys.argv[1]
-        movie_name = sys.argv[2]
-        prices = list(map(int, sys.argv[3].split(",")))
-        quantities = list(map(int, sys.argv[4].split(",")))
-        print("User provided booking details")
-    else:
-        customer_name = "Nagaveni"
-        movie_name = "toxic"
-        prices = [250, 300, 200]
-        quantities = [2, 3, 1]
-        print("Using default booking details")
-    total_tickets = sum(quantities)
-    total_amount = calculate_bill(prices, quantities)
-    status = booking_type(total_amount)
-    print("\n------ Movie Ticket Booking Summary ------")
-    print("Customer Name:", customer_name)
-    print("Movie Name:", movie_name)
-    print("Ticket Prices:", prices)
-    print("Ticket Quantities:", quantities)
-    print("Total Tickets:", total_tickets)
-    print("Total Amount:", total_amount)
-    print("Booking Status:", status)
 if __name__ == "__main__":
-    main()
+    print("Using default booking details")
+    prices = [250, 300, 200]
+    quantities = [2, 3, 1]
+    total = calculate_bill(prices, quantities)
+    status = booking_type(total)
+    print("\n------ Movie Ticket Booking Summary ------")
+    print("Total Amount:", total)
+    print("Booking Status:", status) 
